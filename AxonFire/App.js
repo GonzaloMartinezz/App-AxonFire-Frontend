5230cda877ff464b2bf677b2887a73b22c89a24a
G9890 <jerocarona5@gmail.com>
2026-04-28 15:49:03 -0300

    rehice las notificaciones (edite el app.js y cree una carpeta services)

--- AxonFire/App.js
+++ AxonFire/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Platform } from 'react-native';
+import * as Notifications from 'expo-notifications';
+
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
+
+  useEffect(() => {
+    registerForPushNotifications();
+  }, []);
+
   return (
     <SafeAreaProvider>
       <AppNavigator />
     </SafeAreaProvider>
   );
+}
+
+async function registerForPushNotifications() {
+  await Notifications.requestPermissionsAsync();
+
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('emergency', {
+      name: 'Emergencias',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 1000, 500, 1000],
+      sound: 'default',
+    });
+  }
 }
